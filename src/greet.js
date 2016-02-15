33240a26188a281a7a080e76fee685fb1030ddde
Peter Vliet <plvliet@us.ibm.com>
2016-02-15 13:37:50 -0800

    handles array of multiple names

--- src/greet.js
+++ src/greet.js
@@ -1,5 +1,19 @@
 export function greet(name) {
+  if(Array.isArray(name)) {
+    return handleArray(name);
+  }
   if(!name) name = 'my friend';
   if(name === name.toUpperCase()) return 'HELLO ' + name.toUpperCase() + '!';
-  return 'Hello, ' + name;
+  return "Hello, " + name + '.';
 }
+
+function handleArray(arrayOfNames) {
+    let hi = "Hello, ";
+    
+    if (arrayOfNames.length === 2) return hi + arrayOfNames[0] + " and " + arrayOfNames[1] + ".";
+    arrayOfNames.forEach(function(name, index) {
+      if (index + 1 === arrayOfNames.length) return hi += "and " + name + ".";
+      hi += (name + ", ")
+    });
+    return hi;
+};
--- test/greeting_spec.js
+++ test/greeting_spec.js
@@ -5,14 +5,23 @@ import {greet} from '../src/greet';
 describe('greet spec', () => {
 
   it('interpolates name in a simple greeting', () => {
-      expect(greet('peter')).to.equal('Hello, peter');
+      expect(greet('peter')).to.equal('Hello, peter.');
   });
 
   it('handles nulls with a stand-in', () => {
-      expect(greet()).to.equal('Hello, my friend');
+      expect(greet()).to.equal('Hello, my friend.');
   });
 
   it('returns string in all caps if name is all caps', () => {
       expect(greet('PETER')).to.equal('HELLO PETER!');
+  });
+
+  it('handles array of two names of input', () => {
+      expect(greet(["peter", "paul"])).to.equal('Hello, peter and paul.');
+  });
+
+  it('handles arbitrary number of names in an input', () => {
+      expect(greet(["peter", "paul", "regina", "shannon"])).to.equal('Hello, peter, paul, regina, and shannon.');
+      expect(greet(["peter", "paul", "regina", "shannon", "mike", "tiffany", "jones"])).to.equal('Hello, peter, paul, regina, shannon, mike, tiffany, and jones.');
   });
 });
